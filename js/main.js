84d8d5b5095a093ffbca8718f5a8bb68b9b6b0fe
César Greppin <cesar.greppin@ringier.ch>
2017-12-18 16:31:44 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,170 @@
+jQuery(document).ready(function($) 
+{
+	
+	
+
+
+	
+
+	
+	
+	var q_count;
+
+	//$('.content').hide();
+	$('.content').css('opacity', 0)
+
+	$("button").on('click', function(e) {
+
+		
+		$('.intro').fadeOut();
+		setTimeout(function() 
+		{
+			//$('.content').fadeIn();
+			$('.content').css('opacity', 1)
+			startQuestions();
+
+			setTimeout(function() 
+			{
+				//SCROLL TO AN ELEMENT FUNCTION
+			    $("body, window").animate({ scrollTop: 0 }, 1);
+			}, 250);
+
+		}, 250);
+
+		q_count = $('.question').length;
+
+		
+		
+	});
+
+
+	function startQuestions(){
+
+		for (var i=1; i<=q_count; i++) {
+		    console.log(i);
+		    q_class = '.q' + i;
+		    q_zIndex = i * -1 + 100;
+		    $(q_class).css('zIndex', q_zIndex);
+		}
+
+
+		
+		$(".answer").on('click', function(e) {
+
+			var q_fadePlease = $(this).parent().parent().parent().parent().parent().find('.question');
+
+			setTimeout(function() 
+			{
+				q_fadePlease.fadeOut(function(){
+					q_fadePlease.find('.video-wrapper').remove();		
+				});
+
+				// SCROLL TO AN ELEMENT FUNCTION
+				// $('html, body').animate({
+				//     scrollTop:$('.content').offset().top +2000
+				// }, 1);
+
+				setTimeout(function() 
+				{
+					//SCROLL TO AN ELEMENT FUNCTION
+					$("body, window").animate({ scrollTop: 0 - 100 }, 1);
+				}, 250);
+
+			}, 1000);
+
+
+			// SCROLL TO AN ELEMENT FUNCTION
+				// $('html, body').animate({
+				//     scrollTop:$('.content').offset().top
+				// }, 1);
+
+
+
+		});	
+
+			// SET A DELAY IN A FUNCTION
+		
+		
+
+	}
+	
+	// Rotating Card
+	function rotateCard(btn){
+		var $card = $(btn).closest('.card-container');
+		console.log($card);
+		if($card.hasClass('hover')){
+			$card.removeClass('hover');
+		} else {
+			$card.addClass('hover');
+		}
+	}
+
+	$('.manual-flip').click(function(){
+		rotateCard(this);
+	});
+
+
+	$('#bt_explications').click(function () {
+		$('.quiz .results').fadeOut(300, function () { });
+		$('#explications').fadeIn(300);
+		$('#explications .result').fadeIn(300);
+
+	});
+
+
+
+
+	// THIS IS EXECUTED WHEN EVERYTHING IS LOADED
+	// var everythingLoaded = setInterval(function() {
+	//   if (/loaded|complete/.test(document.readyState)) {
+	//     clearInterval(everythingLoaded);
+	//   }
+	// }, 10);
+
+	// USE THIS IF SOMETHING CANT LOAD CORRECTLY
+	// $(window).load(function(){
+	// });
+
+	// THIS IS DONE AT EVERY SCROLL
+	// $(window).scroll(function (event) {
+ //    });
+
+    // SCROLL TO AN ELEMENT FUNCTION
+    //$('html, body').animate({
+    //    scrollTop:$('.element').offset().top
+    //}, 1000);
+
+    // SCROLL TO A CALCULATED HEIGHT
+	//$('html, body').animate({
+    //      scrollTop:$(scrollHeight).offset().top
+    //}, 1000);
+
+    // SIMPLE CLICK
+ //    $(".menu").click(function(){
+	// });
+	
+    // SET A DELAY IN A FUNCTION
+	// setTimeout(function() 
+	// {
+	// }, 250);
+
+	// KEEP A DIV SQUARE HACK
+	// var largeur = $('.un-quart').width();
+	// $('.un-quart').height(largeur);
+
+ //   	$(window).resize(function(){
+ //   		var largeur = $('.un-quart').width();
+ //   		$('.un-quart').height(largeur);
+ //    });
+
+   	// THIS IS EXECUTED ON RESIZE
+   	// $(window).resize(function(){
+
+    // });
+
+   	// THIS IS EXECUTED DEPENDING ON WINDOW WIDTH
+	// if($(window).width() > 500)
+	// {
+	// }
+
+});
---
+++ questions-reponses.js
@@ -0,0 +1,279 @@
+quiz.addQuestion({ 
+	title: "Entre 2009 et 2015, les statistiques de police montrent que l'évolution du nombre de délits commis par les mineurs…",
+	answers: [
+		{ 
+			text: "est stable, ni plus ni moins",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "est toujours en augmentation, année après année",
+			result: 2,
+			score: 0
+		},
+		{ 
+			text: "est réjouissante parce que quasi toutes les catégories de délits ont diminué de moitié",
+			result: 3,
+			score: 10
+		},
+		{ 
+			text: "est à nuancer, parce que les délits contre le patrimoine diminuent et les délits de violence explosent",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Selon les dernières données criminologiques de 2015, les 15-16 ans…",
+	answers: [
+		{ 
+			text: "ont commis deux fois plus de vols en 2014 qu'il y a 10 ans",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "ont commis plus de deux fois moins de violences qu'il y a 10 ans",
+			result: 2,
+			score: 10
+		},
+		{ 
+			text: "ont surtout fait exploser les actes de vandalisme et le nombre de graffiti par rapport à il y a 10 ans",
+			result: 3,
+			score: 0
+		},
+		{ 
+			text: "ont commis cinq fois moins de violences sexuelles qu'il y a 10 ans",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Selon les dernières données criminologiques de 2015, les 15 - 16 ans...",
+	answers: [
+		{ 
+			text: "de Lausanne sont plus victimes de lésions corporelles que ceux de Zurich (de la part d'adultes comme de mineurs)",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "sont beaucoup moins victimes de vols avec violence et de racket, à Lausanne, en 2014 par rapport à 2004 (de la part d'adultes comme de mineurs)",
+			result: 2,
+			score: 10
+		},
+		{ 
+			text: "de Lausanne sont plus victimes de violences sexuelles, en 2014, que ceux de Zurich (de la part d'adultes comme de mineurs)",
+			result: 3,
+			score: 0
+		},
+		{ 
+			text: "sont de plus en plus victimes de violences sexuelles à Lausanne, mais pas à Zurich (de la part d'adultes comme de mineurs)",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Selon les dernières données criminologiques de 2015, les 15 - 16 ans…",
+	answers: [
+		{ 
+			text: "les jeunes lausannois et zurichois consomment toujours plus de tabac",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "la consommation de cannabis par les jeunes lausannois a diminué de plus de 40% en 10 ans",
+			result: 2,
+			score: 10
+		},
+		{ 
+			text: "la consommation de cannabis augmente tant chez les jeunes lausannois que chez les jeunes zurichois",
+			result: 3,
+			score: 0
+		},
+		{ 
+			text: "les jeunes lausannois fument moins de tabac et de cannabis, mais boivent plus qu'il y a dix ans",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Selon les conclusions des dernières données criminologiques de 2015…",
+	answers: [
+		{ 
+			text: "les jeunes lausannois et zurichois sont toujours moins nombreux à penser "Un vrai homme doit être prêt à frapper quand quelqu’un dit du mal de sa famille"",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "les violences commises par les jeunes lausannois et zurichois sont très fortement corrélées à leur milieu et à leur situation sociale (niveau socio-économique, niveau de formation des parents, contexte migratoire)",
+			result: 2,
+			score: 0
+		},
+		{ 
+			text: "la violence exercée par les parents envers leurs enfants constitue le facteur de risque principal des violences commises par les jeunes lausannois et zurichois",
+			result: 3,
+			score: 10
+		},
+		{ 
+			text: "les facteurs en lien avec l’éducation sont fortement associés à la violence des jeunes lausannois et zurichois",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Selon les conclusions des dernières données criminologiques de 2015…",
+	answers: [
+		{ 
+			text: "les jeunes sont beaucoup plus présents dans l'espace publique",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "on observe une évolution incohérente par rapport à la consommation de substances et à l’adhésion à des groupes violents",
+			result: 2,
+			score: 0
+		},
+		{ 
+			text: "les facteurs tels que le climat scolaire, la motivation scolaire, l'école buissonnière sont des facteurs fortement à risque",
+			result: 3,
+			score: 0
+		},
+		{ 
+			text: "il y a une forte augmentation de la consommation problématique de médias (violence, pornographie, jeux vidéo violents)",
+			result: 4,
+			score: 10
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Vous pensez que si les jeunes qui ont 18 ans en 2015 devaient voter:",
+	answers: [
+		{ 
+			text: "ils accepteraient à une courte majorité l'adhésion de la Suisse à l'Europe",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "ils accepteraient à plus de 60% la dépénalisation du cannabis",
+			result: 2,
+			score: 0
+		},
+		{ 
+			text: "ils refuseraient à une courte majorité le principe de l'éduction sexuelle à l'école",
+			result: 3,
+			score: 10
+		},
+		{ 
+			text: "ils seraient plus de 60% à penser que les citoyens de l'UE doivent être libres de travailler en Suisse et de s'y installer",
+			result: 4,
+			score: 10
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Vous pensez que les jeunes qui ont 18 ans en 2015…",
+	answers: [
+		{ 
+			text: "n'ont pas d'intérêt pour la politique",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "veulent apprendre l'hymne national à l'école à une forte majorité",
+			result: 2,
+			score: 0
+		},
+		{ 
+			text: "sont très confiants quant à l'obtention de la formation professionnelle de leur choix",
+			result: 3,
+			score: 10
+		},
+		{ 
+			text: "sont modernes et ils ne sont plus que 5% à se projeter dans le modèle familial traditionnel (homme au travail et femme à la maison)",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Vous pensez que parmi les jeunes qui ont 18 ans en 2015…",
+	answers: [
+		{ 
+			text: "il existe encore dans notre pays un fort Röchtigraben entre les jeunes vivant en Suisse alémanique et ceux qui vivent en Romandie",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "il apparaît un Polentagraben entre le Tessin et le reste de la Suisse",
+			result: 2,
+			score: 10
+		},
+		{ 
+			text: "ceux qui n'ont pas la nationalité suisse pensent très différemment des nationaux",
+			result: 3,
+			score: 0
+		},
+		{ 
+			text: "une grande partie n'est pas du tout en accord avec les idées de leurs parents",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+quiz.addQuestion({ 
+	title: "Vous pensez que parmi les jeunes qui ont 18 ans en 2015…",
+	answers: [
+		{ 
+			text: "l'immense majorité veut une Suisse qui offre les mêmes chances aux Suisses et aux étrangers",
+			result: 1,
+			score: 0
+		},
+		{ 
+			text: "les Suisses alémaniques sont beaucoup moins ouverts que les Suisses romands à l'idée d'une Suisse qui offre les mêmes chances aux Suisses et aux étrangers",
+			result: 2,
+			score: 0
+		},
+		{ 
+			text: "les tessinois sont les moins ouverts à l'idée d'une Suisse qui offre les mêmes chances aux Suisses et aux étrangers",
+			result: 3,
+			score: 10
+		},
+		{ 
+			text: "plus de 60% pensent qu'il faut être né en Suisse pour pouvoir obtenir la nationalité suisse",
+			result: 4,
+			score: 0
+		},
+	]
+});
+
+
+quiz.addResults([
+		{
+			id: 1,
+			text: "0-20 : Votre perception des jeunes est totalement à l'Ouest et vous ne connaissez pas les jeunes. Vous n'avez aucune confiance en eux. Vous vivez certainement dans un état d'angoisse qui peut nuire gravement à votre santé, vous succombez aveuglément aux préjugés et vous contribuez à détériorer le sentiment de bien-être autour de vous. Conseils: documentez-vous, lisez les résultats du sondage zurichois et ceux du sondage suisse des 17 ans, sevrez-vous de la télévision durant un mois au moins, fréquentez des jeunes et vous verrez qu'ils sont bien plus responsables et moins dangereux que vous ne l'imaginez."
+		},
+		{
+			id: 2,
+			text: "30-50 Attention, votre niveau de confiance en les jeunes est en crise à tel point qu'il montre des signes inquiétants. Vous êtes sur le point de sombrer dans le pessimisme, pire le défaitisme, et de voir le mal partout. Il vous faut consacrer du temps à bien réfléchir aux valeurs auxquelles vous croyez et voulez défendre. Documentez-vous, ne croyez pas que les articles de la presse à sensation sont emblématiques de la jeunesse, ne succombez pas aux discours de certains politiciens qui utilisent les jeunes pour faire de l'électorat. Lorsqu'il y a aura des votations sur des objets concernant les jeunes, n'oubliez jamais le jour où vous avez testé ce quiz."
+		},
+		{
+			id: 3,
+			text: "60-80 Vous êtes normal(e). Votre niveau de confiance en la jeunesse n'est pas encore altéré. Vous avez une certaine clairvoyance relative à la jeunesse et vous savez ne pas faire des généralités face aux assauts des faits divers qui mettent en scène les jeunes, mais vous devez quand même rester    vigilent(e) pour ne pas croire à tort que tout fout le camp dans notre société et que les valeurs auxquelles vous croyez vont disparaître sous peu. Gardez confiance! Allez vers les jeunes, ils vous le rendront bien.Number 2"
+		},
+		{
+			id: 4,
+			text: "90-100 : Bravo, vous vivez dans le vrai monde, vos valeurs et vos représentations sont intactes et en parfaite adéquation avec ce que l'on sait des jeunes. Peut-être êtes-vous souvent en contact avec eux ou même un(e) professionnel(le) du domaine? En tous les cas, continuez, persistez et persuadez tout le monde autour de vous que vos idées sont les bonnes. Vos intuitions sont merveilleuses, la société a besoin de vous, investissez-vous de suite au profit des jeunes, ils ont également besoin de vous! Merci!"
+		},
+	]);
